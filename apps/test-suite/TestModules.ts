6c427132573466f87e91a044a32c35b8e9798041
Wiktor Smaga <wiktor.smaga13@gmail.com>
2025-12-03 16:37:06 +0100

    Fix TestModules

--- apps/test-suite/TestModules.ts
+++ apps/test-suite/TestModules.ts
@@ -86,7 +86,6 @@ export function getTestModules() {
     modules.push(
       require('./tests/Contacts'),
       require('./tests/ContactsNext'),
-      require('./tests/SVG'),
       require('./tests/Localization'),
       require('./tests/Recording'),
       optionalRequire(() => require('./tests/Notifications')),
